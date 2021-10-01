ae114fcf60687854a1db1b2078ced477046efda5
kyh196201 <kyh196201@gmail.com>
2021-10-01 17:57:36 +0900

    fb 버전 추가

---
+++ AWONDER/js/fb__main.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', e => {
+	const mainVisualSwiper = new Swiper('.js__visual__slider', {
+		loop: true,
+	  
+		// pagination: {
+		//   el: '',
+		// },
+	  
+		// navigation: {
+		//   nextEl: '',
+		//   prevEl: '',
+		// },
+	});
+});
