1dfd924290eef19b79321a28c1007521e3b8e3ac
kyh196201 <kyh196201@gmail.com>
2021-10-06 17:09:42 +0900

    AWONDER 중간 배너 영역까지 마크업

--- AWONDER/js/fb__main.js
+++ AWONDER/js/fb__main.js
@@ -16,4 +16,37 @@ window.addEventListener('DOMContentLoaded', e => {
 		loop: true,
 		effect: "fade",
 	});
+
+	const brandSlider = new Swiper('.js__brand__slider', {
+		loop: false,
+
+		pagination: {
+			el: '.js__brand__pagination',
+		},
+	
+		navigation: {
+			nextEl: '.js__brand__next-btn',
+			prevEl: '.js__brand__prev-btn',
+		},
+	});
+
+	const bestProductSlider = new Swiper('.js__best-product__slider', {
+		loop: false,
+
+		slidesPerView: 3,
+		spaceBetween: 60,
+	});
+
+	const middleBannerSlider = new Swiper('.js__middle-banner__slider', {
+		loop: true,
+
+		pagination: {
+			el: '.js__middle-banner__pagination',
+		  },
+		
+		  navigation: {
+			nextEl: '.js__middle-banner__next-btn',
+			prevEl: '.js__middle-banner__prev-btn',
+		  },
+	});
 });
