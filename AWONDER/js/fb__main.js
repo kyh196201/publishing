17ea294c26b378b6f2111bf39bfbf89d8c5d4eab
kyh196201 <kyh196201@gmail.com>
2021-10-07 14:57:09 +0900

    AWONDER BOTTOM BANNER까지 마크업

--- AWONDER/js/fb__main.js
+++ AWONDER/js/fb__main.js
@@ -42,11 +42,54 @@ window.addEventListener('DOMContentLoaded', e => {
 
 		pagination: {
 			el: '.js__middle-banner__pagination',
-		  },
+		},
 		
-		  navigation: {
+		navigation: {
 			nextEl: '.js__middle-banner__next-btn',
 			prevEl: '.js__middle-banner__prev-btn',
-		  },
+		},
+	});
+
+	const wonderClubSlider = new Swiper('.js__wonder-club__slider', {
+		loop: true,
+
+		slidesPerView: 'auto',
+		loopedSlides: 5,
+		spaceBetween: 0,
+		
+		navigation: {
+			nextEl: '.js__wonder-club__next-btn',
+			prevEl: '.js__wonder-club__prev-btn',
+		},
+	});
+
+	const placeSlider = new Swiper('.js__doctors-place__slider', {
+		loop: true,
+
+		slidesPerView: 'auto',
+		centeredSlides: true,
+		loopedSlides: 4,
+		// spaceBetween: 80,
+		
+		navigation: {
+			nextEl: '.js__doctors-place__next-btn',
+			prevEl: '.js__doctors-place__prev-btn',
+		},
+	});
+
+	const eventSlider = new Swiper('.js__event__slider', {
+		loop: false,
+
+		slidesPerView: 2,
+		spaceBetween: 30,
+
+		pagination: {
+			el: '.js__event__pagination',
+		},
+		
+		navigation: {
+			nextEl: '.js__event__next-btn',
+			prevEl: '.js__event__prev-btn',
+		},
 	});
 });
